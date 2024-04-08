7e3fbfaaab6e1e1605da9eed363ea155420bbb74
Couti Darius <coutidarius@gmail.com>
2024-04-08 16:03:03 +0300

    first commit

---
+++ CustomTabBarNavigation/CustomTabBarNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarNavigationApp.swift
+//  CustomTabBarNavigation
+//
+//  Created by Darius Couti on 08.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

d95c2b3633fa058ab1f1614af29a218039be416f
Couti Darius <coutidarius@gmail.com>
2024-04-13 09:44:40 +0300

    add tabbaractions to custom tab bar

--- CustomTabBarNavigation/Views/RootView.swift
+++ CustomTabBarNavigation/Views/RootView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct RootView: View {
     let tabs: [TabBarItem] = [.home, .search, .history, .profile]
+    @State private var actionTabs: [TabBarActionItem] = []
     @State private var selectedTab: TabBarItem = .home
     @State private var selectedScreen = 0
     @State private var showTabBar = true
@@ -21,7 +22,7 @@ struct RootView: View {
                 .transition(.slide)
         }
         .overlay(alignment: .bottom) {
-            CustomTabBar(tabs: tabs, selectedTab: $selectedTab)
+            CustomTabBar(tabs: tabs, selectedTab: $selectedTab, actionTabs: $actionTabs)
                 .hideTabBarAnimation(showTabBar: $showTabBar)
         }
     }
--- CustomTabBarNavigation/Views/TabBar/CustomTabBar.swift
+++ CustomTabBarNavigation/Views/TabBar/CustomTabBar.swift
@@ -10,8 +10,27 @@ import SwiftUI
 struct CustomTabBar: View {
     let tabs: [TabBarItem]
     @Binding var selectedTab: TabBarItem
+    @Binding var actionTabs: [TabBarActionItem]
 
     var body: some View {
+        VStack {
+            if actionTabs.isEmpty {
+                tabBarButtons
+            } else {
+                tabBarActions
+            }
+        }
+        .onAppear {
+            withAnimation {
+                actionTabs = actionTabs
+            }
+        }
+    }
+}
+
+extension CustomTabBar {
+    
+    var tabBarButtons: some View {
         HStack(spacing: 40) {
             ForEach(tabs, id: \.self) { tab in
                 tab.tabImage()
@@ -23,10 +42,27 @@ struct CustomTabBar: View {
                     .foregroundStyle(selectedTab == tab ? .blue : .gray)
             }
         }
-            .frame(height: 60)
-            .padding([.top, .bottom], 10)
-            .padding([.leading, .trailing], 30)
-            .background(Color.white)
-            .clipShape(.rect(cornerRadius: 20))
+        .frame(height: 60)
+        .padding([.top, .bottom], 10)
+        .padding([.leading, .trailing], 30)
+        .background(Color.white)
+        .clipShape(.rect(cornerRadius: 20))
+    }
+    
+    var tabBarActions: some View {
+        HStack(spacing: 40) {
+            ForEach(actionTabs, id: \.self) { actionTabs in
+                actionTabs.tabImage()
+                    .onTapGesture {
+                        //custom function
+                    }
+            }
+        }
+        .frame(height: 60)
+        .padding([.top, .bottom], 10)
+        .padding([.leading, .trailing], 30)
+        .background(Color.white)
+        .clipShape(.rect(cornerRadius: 20))
+        .transition(.move(edge: .trailing))
     }
 }
